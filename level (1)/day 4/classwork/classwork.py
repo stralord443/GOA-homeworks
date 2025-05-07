5d5174e911791b4d7a8fcf889abf713b339ab942
stralord443 <goguna19@gmail.com>
2025-05-07 18:57:04 +0400

    fivth commit

---
+++ level (1)/day 4/classwork/classwork.py
@@ -0,0 +1,20 @@
+
+# 1) შექმენით ხუთი ცვლადი და გამოიყენეთ ეს ცვლადები რომ შეადგინოთ წინადადება
+# 2) დაარქვით ამ ცვლადებს snake_case სინტაქსით
+
+my_name="goga"
+my_surname="adamashvili"
+my_age=18
+my_hobby="gaming"
+my_dream="become proggramer"
+
+# 3) მომხმარებელს შეატანინეთ მისი სახელი გვარი და ასაკი და შემდეგ გამოიტანეთ ისინი outpout-ში 
+# შედეგი გამოსახულებით სახელი: {შემოყვანილი სახელი} (იგივე ყველა ცვლადზე).
+
+name=input()
+surname=input()
+age=input()
+
+print(name)
+print(surname)
+print(age)
