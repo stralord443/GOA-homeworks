ed175578e0e4ebe9cbb9b88fe60b832cc9934ae1
stralord443 <goguna19@gmail.com>
2025-06-16 14:28:10 +0400

    fortenth commit

---
+++ homework/day 15/homework/homework.py
@@ -0,0 +1,14 @@
+# 1) კომენტარებით ახსენით რისთვის გამოიყენება Len()-ფუნქცია
+# 2) კომენტარებით ახსენით რისთვის გამოიყენება List-ი
+# 3) კომენტარებით ახსენით რა განსხვავებაა List-სა და ცვლადებ-ს შორის
+# 4) რისი შენახვა შეუძლია list-ს?
+# 5) შექმენი ლისტი, შეინახეთ სამი წიგნის სახელი. დაბეჭდე მთელი სია და დაბეჭდე, რამდენი წიგნია მასში.
+
+# len finction allows us to know low long is our list
+# list is user for containing information
+# list allows us to contain any kind of information and variable allow us to contain only one datatype
+
+X=["შავი ისარი", "მოგზაურობა ჩრდილოეთის რეისზე", "პიტერ პენი"]
+
+print(X)
+print(len(X))
