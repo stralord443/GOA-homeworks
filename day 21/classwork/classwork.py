43608a4a6aae52c7086df1045e6fe125ccec921f
stralord443 <goguna19@gmail.com>
2025-07-13 17:19:25 +0400

    twentyfirst commit

---
+++ day 21/classwork/classwork.py
@@ -0,0 +1,8 @@
+#1) ახსენით რა განსხვავებაა index-ისა და slising შორის,  მომხმარებელმა შეიყვანოს სიტყვა,
+# და გამოიტანე მისგან მესამეიდან მეხუთე სიმბოლომდე ნაწყვეტი
+
+# indexing helps us to make more detailed changes to our lists
+
+X=input("enter a word ")
+
+print(X[2:4])
